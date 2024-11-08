a35e1b2322f7af4c8b1279847fff768b3b6a1774
9diin <weartstudio@naver.com>
2024-11-08 16:48:32 +0900

    feat: 디테일 기능 잡기

--- src/components/common/navigation/SideNavigation.tsx
+++ src/components/common/navigation/SideNavigation.tsx
@@ -19,13 +19,10 @@ function SideNavigation() {
 
     // 페이지 생성 및 Supabase 연동
     const onCreate = async () => {
-        console.log("함수호출");
-        router.push("/create");
-
         // Supabase 데이터베이스 row 생성
         const { error, status } = await supabase
             .from("todos")
-            .insert([{ title: "", start_date: "", end_date: "", contents: [] }])
+            .insert([{ title: "", start_date: new Date(), end_date: new Date(), contents: [] }])
             .select();
 
         if (error) {
@@ -36,6 +33,11 @@ function SideNavigation() {
                 title: "생성 완료!",
                 description: "새로운 투두리스트가 생성 되었습니다.",
             });
+
+            // 방금 생성한 TO DO LIST의 ID 값으로 URL 파라미터 생성
+            let { data } = await supabase.from("todos").select();
+            if (data) router.push(`/create/${data[data?.length - 1].id}`);
+            getTodos();
         }
     };
 
